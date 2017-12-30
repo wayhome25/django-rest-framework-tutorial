0e2dfd79f49a980cc239cd87d87784891ff3f901
Lee HyunJoo <wayhome250@gmail.com>
2017-12-30 19:06:24 +0900

    creat endpoints for root and highlighted snippets

--- snippets/views.py
+++ snippets/views.py
@@ -1,5 +1,9 @@
 from rest_framework import generics
 from rest_framework import permissions
+from rest_framework import renderers
+from rest_framework import reverse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from django.contrib.auth.models import User
 
@@ -7,6 +11,22 @@ from snippets.models import Snippet
 from snippets.permissions import IsOwnerOrReadOnly
 from snippets.serializers import SnippetSerializer
 from snippets.serializers import UserSerializer
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+    })
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_calsses = (renderers.StaticHTMLRenderer,)
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
 
 
 class SnippetList(generics.ListCreateAPIView):
--- tutorial/urls.py
+++ tutorial/urls.py
@@ -20,9 +20,11 @@ from django.contrib import admin
 from snippets import views
 
 urlpatterns = [
+    url(r'^$', views.api_root),
     url(r'^admin/', admin.site.urls),
     url(r'^users/$', views.UserList.as_view()),
     url(r'^users/(?P<pk>\d+)/$', views.UserDetail.as_view()),
     url(r'^snippets/', include('snippets.urls')),
+    url(r'^snippets/(?P<pk>\d+)/highlight/$', views.SnippetHighlight.as_view()),
     url(r'^api-auth/', include('rest_framework.urls')),
 ]
